039d3767561a22ec30571b9035a0d612b13929a7
西竜太郎 <r-nishi@nishiryuutarounoMacBook-Air.local>
2022-06-04 23:42:20 +0900

    改善案を追加

--- みおちゃん.php
+++ みおちゃん.php
@@ -2,36 +2,35 @@
 
 class みおちゃん
 {
-	private $count = 0;
-	
-	public function plus($x, $y)
+	private int $count = 0;
+
+    /**
+     * @param int $x
+     * @param int $y
+     * @return int|string
+     */
+	public function plus(int $x, int $y): int|string
 	{
 		// 計算回数
 		$this->count++;
-
 
  		// 計算回数が3の倍数の時にみそスープ飲みたくなる
 		if($this->count % 3 === 0){
 			return 'みそスープ';
 		}
-
 		return $x + $y;
 	}
 }
-
 
 // インスタンスを使いまわして5回計算
 $みおちゃん = new みおちゃん();
 for($i = 0; $i < 5; $i++){
 	echo $みおちゃん->plus(1, 1);
+    echo PHP_EOL;
 }
-
-echo PHP_EOL;
 
 // 毎回インスタンスを生成して5回計算
 for($i = 0; $i < 5; $i++){
 	echo (new みおちゃん())->plus(1, 1);
+    echo PHP_EOL;
 }
-
-echo PHP_EOL;
-
---
+++ みおちゃん改.php
@@ -0,0 +1,27 @@
+<?php
+
+class みおちゃん改
+{
+    /**
+     * @param int $x
+     * @param int $y
+     * @param int $count 計算回数
+     * @return int|string
+     */
+	public function plus(int $x, int $y, int $count): int|string
+    {
+ 		// 計算回数が3の倍数の時にみそスープ飲みたくなる
+		if ($count % 3 === 0) {
+			return 'みそスープ';
+		}
+		return $x + $y;
+	}
+}
+
+
+// インスタンスを使いまわして5回計算
+$みおちゃん改 = new みおちゃん改();
+for ($i = 1; $i < 6; $i++) {
+	echo $みおちゃん改->plus(1, 1, $i);
+    echo PHP_EOL;
+}
